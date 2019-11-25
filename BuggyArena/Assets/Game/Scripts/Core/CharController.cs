78339401f121afd9e895b893479acab73e471a12
Adrian Restu Putranto <adrian.restup@gmail.com>
2019-11-25 08:46:45 +0700

    - Add probuilder - Add Do Tween - Add maple character - Script character controller

---
+++ BuggyArena/Assets/Game/Scripts/Core/CharController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class CharController : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 1f;
+    [SerializeField] float jumpForce = 10f;
+    [SerializeField] float gravity = 5f;
+    [SerializeField] CharacterController characterController;
+    [SerializeField] Animator animator;
+    [SerializeField] Rigidbody rigBody;
+
+    Vector3 movement, moveDirection;
+    float jumpDir;
+    
+    void Update()
+    {
+        float xAxis = Input.GetAxis("Horizontal");
+        float zAxis = Input.GetAxis("Vertical");
+        bool jump = Input.GetKeyDown(KeyCode.Space);
+
+        movement = new Vector3(xAxis, 0, zAxis);
+        movement = movement * moveSpeed;
+        moveDirection = movement;
+
+        if (characterController.isGrounded)
+        {
+            jumpDir = -1;
+            if (jump)
+            {
+                jumpDir = jumpForce;
+            }
+        }
+
+        jumpDir -= gravity * Time.deltaTime;
+        moveDirection.y = jumpDir;
+
+        float moveValue = (xAxis != 0f || zAxis != 0f) ? 1f : 0f;
+        animator.SetFloat("Move", moveValue);
+        Debug.Log(characterController.isGrounded.ToString());
+    }
+
+     void FixedUpdate()
+     {
+         characterController.Move(moveDirection * Time.deltaTime);
+        if (movement != Vector3.zero)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(movement);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 10f);
+        }
+    }
+    
+}
---
+++ BuggyArena/Assets/Game/Scripts/Core/TopDownCamera.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownCamera : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] private float height = 10f;
+    [SerializeField] private float distance = 20f;
+    [SerializeField] private float angle = 45f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleCamera();
+    }
+
+    void HandleCamera()
+    {
+        if (target)
+        {
+            
+            
+        }
+    }
+}
