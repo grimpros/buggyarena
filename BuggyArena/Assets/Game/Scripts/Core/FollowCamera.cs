e7b3c45eec36dfdc151d43a6aa6c4a56934aba8c
Adrian Restu Putranto <adrian.restup@gmail.com>
2019-11-27 07:28:24 +0700

    Add follow camera script

---
+++ BuggyArena/Assets/Game/Scripts/Core/FollowCamera.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum CameraType
+{
+    Vertical, Horizontal, FreeCam
+}
+
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] float camSpeed = 1f;
+    [SerializeField] CameraType cameraType;
+    [SerializeField] Vector3 offset;
+
+    void FixedUpdate()
+    {
+        if (target != null)
+        {
+            FollowTarget();
+        }
+    }
+
+    void FollowTarget()
+    {
+        Vector3 toPos = target.position + offset;
+        switch (cameraType)
+        {
+                case CameraType.Vertical:
+                    toPos.x = 0f;
+                    break;
+                
+                case CameraType.Horizontal:
+                    toPos.z = 0f;
+                    break;
+        }
+
+        Vector3 smoothedPos = Vector3.Lerp(transform.position, toPos, camSpeed);
+        transform.position = smoothedPos;
+    }
+}
